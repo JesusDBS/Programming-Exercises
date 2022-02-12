5f3c4a6228bea67c3bbcd53a2109dd47559f049a
JesusDBS <jesusbingucv@gmail.com>
2022-02-12 13:27:05 -0400

    [ADD] JS folder, excercises

---
+++ 1/js/2.js
@@ -0,0 +1,42 @@
+/* Average:
+
+In this exercise you will create a program that computes the average of a collection
+of values entered by the user. The user will enter 0 as a sentinel value to indicate
+that no further values will be provided. Your program should display an appropriate
+error message if the first value entered by the user is 0. */
+
+const prompt = require("prompt-sync")({ sigint: true});
+
+function computeAverage(array){
+    let sum = 0;
+
+    for (let i=0; i < array.length; i++){
+        sum += array[i];
+    }
+    let average = sum/(array.length);
+
+    return average;
+}   
+
+
+const numbers = [];
+
+while(true){
+    let number = Number(prompt("Enter a number "));
+
+    if (number === 0 && numbers.length === 0){
+        console.log("The first number can't be zero");
+        continue;
+
+    } else if (number === 0 && numbers.length != 0){
+        console.log("Bye!");
+        break;
+        
+    } else {
+        numbers.push(number);
+        let average = computeAverage(numbers);
+
+        console.log(`The average is ${average}`);
+    }
+
+}
