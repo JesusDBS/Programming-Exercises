0a910340380945b4fc991e13f48d8c47f8215511
JesusDBS <jesusbingucv@gmail.com>
2022-03-04 17:12:35 -0400

    [ADD] problem-03-Fibonacci-Sequence

---
+++ python/Problem-03-Fibonacci-Sequence/Fibonacci-Sequence.py
@@ -0,0 +1,5 @@
+#Fibonacci Sequence:
+# Write a program that prints the first 50 numbers of the Fibonacci sequence starting at 0.
+#   - The Fibonacci series consists of a succession of numbers in which the next number is always the sum of the previous two.
+#   -0, 1, 1, 2, 3, 5, 8, 13...
+
