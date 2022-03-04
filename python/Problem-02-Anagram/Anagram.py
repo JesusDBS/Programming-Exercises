4a66ea34592466aae51211d6276a6461c323204a
JesusDBS <jesusbingucv@gmail.com>
2022-03-03 22:59:23 -0400

    [ADD] Problem-02-Anagram

---
+++ python/Problem-02-Anagram/Anagram.py
@@ -0,0 +1,7 @@
+#Anagram:
+#Write a function that receives two words (String) and returns true or false (Bool) 
+# depending on whether they are anagrams or not.
+    # - An Anagram consists of forming a word by rearranging ALL the letters of another initial word.
+    # - It is NOT necessary to check that both words exist.
+    # - Two words exactly alike are not anagrams.
+
