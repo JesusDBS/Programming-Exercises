15bb29e28edddb8469573a563da2e638a5d33e2e
JesusDBS <jesusbingucv@gmail.com>
2022-05-20 19:03:45 -0400

    [ADD] python Problem-06-Aspect_Ratio

---
+++ python/Problem-06-Aspect_Ratio/Aspect_Ratio.py
@@ -0,0 +1,12 @@
+#Aspect Ratio:
+# Create a program that calculates the aspect ratio of an image from a url.
+#  - Example url: https://raw.githubusercontent.com/mouredev/mouredev/master/mouredev_github_profile.png
+#  - By ratio we refer for example to the "16:9" of a 1920*1080px image.
+
+import io
+import requests
+
+from PIL import Image
+
+
+
