c6ce9594b5765caed21c48433f4352b22934c46b
JesusDBS <jesusbingucv@gmail.com>
2022-05-23 11:32:34 -0400

    [IMP] program functions template

--- python/Problem-08-Counting_Words/counting_words.py
+++ python/Problem-08-Counting_Words/counting_words.py
@@ -2,11 +2,24 @@
 #Create a program that counts how many times each word is repeated and displays the final count of all of them
 # - Punctuation marks are not part of the word.
 # - A word is the same even if it appears in upper and lower case.
-#- You cannot use language functions that solve it automatically.
+# - You cannot use language functions that solve it automatically.
+
+def count_words(string):
+    """Counts the words contained in the string inserted by the user.
+
+    Parameters
+    ----------
+    string: str
+    """
+    #validations
+    assert any(string), "The string can't be an empty string!"
+    pass
+
 
 def main():
-    pass
+    string = input("Insert your string: ")
+    print(count_words(string))
 
 
 if __name__ == "__main__":
-    pass
+    main()
