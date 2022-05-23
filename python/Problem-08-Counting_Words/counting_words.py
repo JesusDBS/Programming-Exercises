b2225c8a68d6f7b20b6e1c119986b931a1ddded7
JesusDBS <jesusbingucv@gmail.com>
2022-05-22 22:10:18 -0400

    [ADD] python Problem-08-Counting_Words

---
+++ python/Problem-08-Counting_Words/counting_words.py
@@ -0,0 +1,12 @@
+#Counting Words:
+#Create a program that counts how many times each word is repeated and displays the final count of all of them
+# - Punctuation marks are not part of the word.
+# - A word is the same even if it appears in upper and lower case.
+#- You cannot use language functions that solve it automatically.
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    pass
