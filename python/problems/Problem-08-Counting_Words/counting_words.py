b92480a5d03153f87267ee5addc1df5ea4980a0f
JesusDBS <jesusbingucv@gmail.com>
2022-05-24 23:42:52 -0400

    [SOLVED] python Problem-08-Counting_Words

--- python/problems/Problem-08-Counting_Words/counting_words.py
+++ python/problems/Problem-08-Counting_Words/counting_words.py
@@ -11,6 +11,8 @@ Purpose: Count words.
 # - You cannot use language functions that solve it automatically.
 
 #---------------------------------------------------
+import re
+
 def count_words(string):
     """Counts the words contained in the string inserted by the user.
 
@@ -20,13 +22,25 @@ def count_words(string):
     """
     #validations
     assert any(string), "The string can't be an empty string!"
-    pass
+
+    #Cleaning string
+    string = list(string.lower().split(' '))
+    string = list(filter(lambda word: word, string))
+    string = [re.sub("[^a-zA-Z]+", "",character) for character in string]
+
+    words = dict.fromkeys(set(string), 0)
+
+    #Counting words
+    for key in words.keys():
+        words[key] = len(list(filter(lambda word: word == key, string)))
+
+    for key, val in words.items():
+        print(f'The word "{key}" appears {val} times in your string!')
 
 #----------------------------------------------------
 def main():
     string = input("Insert your string: ")
-    print(count_words(string))
-
+    count_words(string)
 #----------------------------------------------------
 if __name__ == "__main__":
     main()
