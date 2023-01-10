3adb105e47f91a311de19fb5e40937b3a9673ba1
JesusDBS <jesusbingucv@gmail.com>
2023-01-10 09:39:23 -0400

    [ADD] Problem-13-budget-app

---
+++ python/problems/Problem-13-budget-app/budget_app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Author: Jesús David Briceño <jesusbingucv@gmail.com>
+Purpose: A simple budget class.
+"""
+# ---------------------------------------------------
+# Budget App:
+# Create a Budget class that can instantiate objects based on different budget categories like food, clothing,
+# and entertainment. These objects should allow for depositing and withdrawing funds from each category,
+# as well computing category balances and transferring balance amounts between categories.
+# ---------------------------------------------------
+
+
+class BudgetApp:
+    pass
---
+++ python/problems/Problem-13-budget-app/tests.py
@@ -0,0 +1,10 @@
+import unittest
+from budget_app import BudgetApp
+
+
+class TestBudgetApp(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
