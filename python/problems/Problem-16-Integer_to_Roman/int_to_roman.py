bbb02f334fcd2ae168d2b73a2d748a55c32c4370
JesusDBS <jesusbingucv@gmail.com>
2023-04-23 17:49:48 -0400

    [UPDATE] only working with digits

--- python/problems/Problem-16-Integer_to_Roman/int_to_roman.py
+++ python/problems/Problem-16-Integer_to_Roman/int_to_roman.py
@@ -5,12 +5,14 @@
 
 class IntegerToRoman(int):
     """
-    Simple class for convert integer numbers to roman base numbers
+    Simple class for convert integer numbers to roman base numbers.
     """
     roman_numbers = {
         0: 'N',
         1: 'I',
+        4: 'IV',
         5: 'V',
+        9: 'IX',
         10: 'X',
         50: 'L',
         100: 'C',
@@ -24,6 +26,13 @@ class IntegerToRoman(int):
         """
         if (number := self.__check_if_number_is_base()):
             return self.roman_numbers[number]
+        
+        if number == 0 and type(number) == int:
+            return self.roman_numbers[number]
+        
+        if len(str(self)) == 1:
+            return self.__convert_digit()
+
         return False
 
     def __check_if_number_is_base(self):
@@ -32,4 +41,25 @@ class IntegerToRoman(int):
                 if number == self:
                     return number
         return False
+
+    def __integer_to_string(self):
+        number = str(self)
+        return number
+    
+    def __convert_digit(self):
+        number = self
+        if len(str(number)) == 1:
+            if 1 < number < 5:
+                if (number:=number // 1) <= 3:
+                    return self.roman_numbers[1]*number
+            
+            if 5 < number < 9:
+                if (number:=number % 5) <= 3:
+                    return self.roman_numbers[5] + self.roman_numbers[1]*number
+
+        return False
+
+        
+
+    
 
--- python/problems/Problem-16-Integer_to_Roman/tests.py
+++ python/problems/Problem-16-Integer_to_Roman/tests.py
@@ -13,9 +13,41 @@ class TestIntegerToRoman(unittest.TestCase):
     def test_integer_to_roman_is_integer_instance(self):
         self.assertTrue(isinstance(self.int_to_roman, int))
 
+    def test_check_convert_zero(self):
+        zero = IntegerToRoman(0)
+        self.assertEqual(zero.convert_to_roman(), 'N')
+
     def test_convert_to_roman_base_numbers(self):
         number = 'X'
         self.assertEqual(self.int_to_roman.convert_to_roman(), number)
+
+    def test_convert_4_to_roman(self):
+        four = IntegerToRoman(4)
+        self.assertEqual(four.convert_to_roman(), 'IV')
+
+    def test_convert_9_to_roman(self):
+        nine = IntegerToRoman(9)
+        self.assertEqual(nine.convert_to_roman(), 'IX')
+
+    def test_convert_2_to_roman(self):
+        two = IntegerToRoman(2)
+        self.assertEqual(two.convert_to_roman(), 'II')
+
+    def test_convert_3_to_roman(self):
+        three = IntegerToRoman(3)
+        self.assertEqual(three.convert_to_roman(), 'III')
+
+    def test_convert_6_to_roman(self):
+        six = IntegerToRoman(6)
+        self.assertEqual(six.convert_to_roman(), 'VI')
+
+    def test_convert_7_to_roman(self):
+        seven = IntegerToRoman(7)
+        self.assertEqual(seven.convert_to_roman(), 'VII')
+
+    def test_convert_8_to_roman(self):
+        eight = IntegerToRoman(8)
+        self.assertEqual(eight.convert_to_roman(), 'VIII')
 
 
 if __name__ == "__main__":
