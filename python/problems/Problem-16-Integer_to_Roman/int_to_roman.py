332e17e8c18d4d39dbd8457122dbbe3988dfe638
JesusDBS <jesusbingucv@gmail.com>
2023-04-24 12:23:20 -0400

    [UPDATE] convert two digit numbers in roman

--- python/problems/Problem-16-Integer_to_Roman/int_to_roman.py
+++ python/problems/Problem-16-Integer_to_Roman/int_to_roman.py
@@ -31,23 +31,32 @@ class IntegerToRoman(int):
             return self.roman_numbers[number]
         
         if len(str(self)) == 1:
-            return self.__convert_digit()
+            return self.__convert_digit(number=self)
+
+        if len(str(self)) == 2:
+            return self.__convert_2_digits_number(number=self)
+            
+
+        #     pass
+        # Use divmod
 
         return False
 
-    def __check_if_number_is_base(self):
-        if self in self.roman_numbers.keys():
-            for number in self.roman_numbers.keys():
-                if number == self:
-                    return number
+    def __check_if_number_is_base(self, number=None):
+        if not number:
+            number = self
+       
+        if number in self.roman_numbers.keys():
+            for _number in self.roman_numbers.keys():
+                if _number == number:
+                    return _number
         return False
 
     def __integer_to_string(self):
         number = str(self)
         return number
     
-    def __convert_digit(self):
-        number = self
+    def __convert_digit(self, number):
         if len(str(number)) == 1:
             if 1 < number < 5:
                 if (number:=number // 1) <= 3:
@@ -58,8 +67,64 @@ class IntegerToRoman(int):
                     return self.roman_numbers[5] + self.roman_numbers[1]*number
 
         return False
+    
+    def __convert_2_digits_number(self, number):
+        #TODO: refactor this method
 
-        
+        if len(str(number)) == 2:
+            quotient, remainder = divmod(number, 10)
+            if 1 <= quotient < 4:
+                if (number := self.__check_if_number_is_base(number=remainder)):
+                    return self.roman_numbers[10]*quotient + self.roman_numbers[number]
+                
+                if not remainder:
+                    return self.roman_numbers[10]*quotient
+                
+                return self.roman_numbers[10]*quotient + self.__convert_digit(remainder)
+         
+            if quotient == 4:
+                fourty = self.roman_numbers[10] + self.roman_numbers[50]
+
+                if (number := self.__check_if_number_is_base(number=remainder)):
+                    return fourty + self.roman_numbers[number]
+                
+                if not remainder:
+                    return fourty
+                
+                return fourty + self.__convert_digit(number=remainder)
+            
+            if 5 <= quotient < 10 and self <= 59:
+                fifty = self.roman_numbers[50]
+                
+                if (number := self.__check_if_number_is_base(number=remainder)):
+                    return fifty + self.roman_numbers[number]
+                
+                if not remainder:
+                    return fifty
+                
+                return fifty + self.__convert_digit(number=remainder)
+            
+            if 60 <= self <= 99:
+                quotient, remainder = divmod(number, 50)
+                base = self.roman_numbers[50]
+
+                if (num := remainder // 10) <= 3:
+                    base += self.roman_numbers[10]*num
+                
+                else:
+                    base = self.roman_numbers[10] + self.roman_numbers[100]
+
+                if (resto:=remainder % 10) == 0:
+                    return base
+            
+                if 1 <= resto <= 9:
+                    if (number := self.__check_if_number_is_base(number=resto)):
+                        return base + self.roman_numbers[number]
+                    
+                    return base + self.__convert_digit(resto) 
+         
+            
+        return False
 
     
 
--- python/problems/Problem-16-Integer_to_Roman/tests.py
+++ python/problems/Problem-16-Integer_to_Roman/tests.py
@@ -49,6 +49,152 @@ class TestIntegerToRoman(unittest.TestCase):
         eight = IntegerToRoman(8)
         self.assertEqual(eight.convert_to_roman(), 'VIII')
 
+    def test_convert_11_to_roman(self):
+        number = IntegerToRoman(11)
+        self.assertEqual(number.convert_to_roman(), 'XI')
+
+    def test_convert_13_to_roman(self):
+        number = IntegerToRoman(13)
+        self.assertEqual(number.convert_to_roman(), 'XIII')
+
+    def test_convert_14_to_roman(self):
+        number = IntegerToRoman(14)
+        self.assertEqual(number.convert_to_roman(), 'XIV')
+
+    def test_convert_17_to_roman(self):
+        number = IntegerToRoman(17)
+        self.assertEqual(number.convert_to_roman(), 'XVII')
+
+    def test_convert_18_to_roman(self):
+        number = IntegerToRoman(18)
+        self.assertEqual(number.convert_to_roman(), 'XVIII')
+
+    def test_convert_19_to_roman(self):
+        number = IntegerToRoman(19)
+        self.assertEqual(number.convert_to_roman(), 'XIX')
+
+    def test_convert_20_to_roman(self):
+        number = IntegerToRoman(20)
+        self.assertEqual(number.convert_to_roman(), 'XX')
+
+    def test_convert_21_to_roman(self):
+        number = IntegerToRoman(21)
+        self.assertEqual(number.convert_to_roman(), 'XXI')
+
+    def test_convert_24_to_roman(self):
+        number = IntegerToRoman(24)
+        self.assertEqual(number.convert_to_roman(), 'XXIV')
+
+    def test_convert_27_to_roman(self):
+        number = IntegerToRoman(27)
+        self.assertEqual(number.convert_to_roman(), 'XXVII')
+
+    def test_convert_29_to_roman(self):
+        number = IntegerToRoman(29)
+        self.assertEqual(number.convert_to_roman(), 'XXIX')
+
+    def test_convert_30_to_roman(self):
+        number = IntegerToRoman(30)
+        self.assertEqual(number.convert_to_roman(), 'XXX')
+
+    def test_convert_33_to_roman(self):
+        number = IntegerToRoman(33)
+        self.assertEqual(number.convert_to_roman(), 'XXXIII')
+
+    def test_convert_39_to_roman(self):
+        number = IntegerToRoman(39)
+        self.assertEqual(number.convert_to_roman(), 'XXXIX')
+
+    def test_convert_40_to_roman(self):
+        number = IntegerToRoman(40)
+        self.assertEqual(number.convert_to_roman(), 'XL')
+
+    def test_convert_41_to_roman(self):
+        number = IntegerToRoman(41)
+        self.assertEqual(number.convert_to_roman(), 'XLI')
+
+    def test_convert_43_to_roman(self):
+        number = IntegerToRoman(43)
+        self.assertEqual(number.convert_to_roman(), 'XLIII')
+
+    def test_convert_44_to_roman(self):
+        number = IntegerToRoman(44)
+        self.assertEqual(number.convert_to_roman(), 'XLIV')
+
+    def test_convert_49_to_roman(self):
+        number = IntegerToRoman(49)
+        self.assertEqual(number.convert_to_roman(), 'XLIX')
+
+    def test_convert_50_to_roman(self):
+        number = IntegerToRoman(50)
+        self.assertEqual(number.convert_to_roman(), 'L')
+
+    def test_convert_51_to_roman(self):
+        number = IntegerToRoman(51)
+        self.assertEqual(number.convert_to_roman(), 'LI')
+
+    def test_convert_58_to_roman(self):
+        number = IntegerToRoman(58)
+        self.assertEqual(number.convert_to_roman(), 'LVIII')
+
+    def test_convert_59_to_roman(self):
+        number = IntegerToRoman(59)
+        self.assertEqual(number.convert_to_roman(), 'LIX')
+
+    def test_convert_60_to_roman(self):
+        number = IntegerToRoman(60)
+        self.assertEqual(number.convert_to_roman(), 'LX')
+
+    def test_convert_70_to_roman(self):
+        number = IntegerToRoman(70)
+        self.assertEqual(number.convert_to_roman(), 'LXX')
+
+    def test_convert_80_to_roman(self):
+        number = IntegerToRoman(80)
+        self.assertEqual(number.convert_to_roman(), 'LXXX')
+
+    def test_convert_90_to_roman(self):
+        number = IntegerToRoman(90)
+        self.assertEqual(number.convert_to_roman(), 'XC')
+
+    def test_convert_91_to_roman(self):
+        number = IntegerToRoman(91)
+        self.assertEqual(number.convert_to_roman(), 'XCI')
+
+    def test_convert_93_to_roman(self):
+        number = IntegerToRoman(93)
+        self.assertEqual(number.convert_to_roman(), 'XCIII')
+
+    def test_convert_92_to_roman(self):
+        number = IntegerToRoman(92)
+        self.assertEqual(number.convert_to_roman(), 'XCII')
+
+    def test_convert_94_to_roman(self):
+        number = IntegerToRoman(94)
+        self.assertEqual(number.convert_to_roman(), 'XCIV')
+
+    def test_convert_96_to_roman(self):
+        number = IntegerToRoman(96)
+        self.assertEqual(number.convert_to_roman(), 'XCVI')
+
+    def test_convert_97_to_roman(self):
+        number = IntegerToRoman(97)
+        self.assertEqual(number.convert_to_roman(), 'XCVII')
+
+    def test_convert_98_to_roman(self):
+        number = IntegerToRoman(98)
+        self.assertEqual(number.convert_to_roman(), 'XCVIII')
+
+    def test_convert_99_to_roman(self):
+        number = IntegerToRoman(99)
+        self.assertEqual(number.convert_to_roman(), 'XCIX')
+
+    def test_convert_100_to_roman(self):
+        number = IntegerToRoman(100)
+        self.assertEqual(number.convert_to_roman(), 'C')
+
+
+
 
 if __name__ == "__main__":
     unittest.main()
