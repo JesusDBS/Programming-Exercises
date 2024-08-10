fb423c41f5eceadfa643c524553fc2fb36b5f8ea
Jesús Briceño <jesusbingucv@gmail.com>
2024-08-10 13:37:03 -0400

    [ADD] problem 17

---
+++ python/problems/Problem-17-Simple_Library_System/main.py
@@ -0,0 +1,14 @@
+#-------------------------------------------
+# Library Management System
+# Create a system to manage a library's book inventory, borrowers, and borrowing transactions.
+
+# Classes to implement: Book, Member, Librarian, Library.
+# Features:
+# 1) Add, remove, and search for books.
+# 2) Register new members.
+# 3) Borrow and return books.
+# 4) Track borrowing history.
+#-------------------------------------------
+
+if __name__ == '__main__':
+    pass
