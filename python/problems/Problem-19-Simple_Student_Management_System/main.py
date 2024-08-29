1592049b8c3df0e94fa9220850d8d5be10308330
Jesús Briceño <jesusbingucv@gmail.com>
2024-08-29 16:07:29 -0400

    [ADD] Problem 19 scaffold

---
+++ python/problems/Problem-19-Simple_Student_Management_System/main.py
@@ -0,0 +1,11 @@
+# ---------------------------------------------------------------
+# Student Management System
+# Develop an application to manage student records for a school or college.
+
+# Classes to implement: Student, Course, Instructor, Department.
+# Features:
+# Add, update, and delete student information.
+# Enroll students in courses.
+# Assign instructors to courses.
+# Generate student grade reports.
+# ---------------------------------------------------------------
